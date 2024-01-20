06c50090534d7a9e18142af4d57bb635a3085edf
Lianghui Zhu <1653673717@qq.com>
2024-01-20 12:01:29 +0800

    [fix] path in script.

--- vim/scripts/pt-vim-t.sh
+++ vim/scripts/pt-vim-t.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-bash /client-tools/repair_A100.sh
-source /opt/conda/bin/activate /home/zhulianghui/.conda/envs/deit-dev
-cd /share/project/lianghuizhu/JudgeLM-Project/old_ckpts/VisionProjects/deit;
+conda activate <your_env>
+cd <path_to_Vim>/vim;
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual_with_cls_token --batch-size 128 --num_workers 25 --data-path /share/project/lianghuizhu/datasets/IN1K --output_dir ./output/vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual --no_amp > /share/project/lianghuizhu/JudgeLM-Project/old_ckpts/VisionProjects/deit/output/vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual.txt 2>&1
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual_with_cls_token --batch-size 128 --num_workers 25 --data-path <path_to_IN1K_dataset> --output_dir ./output/vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual --no_amp
