912f71d1d3454680f90ae7e99a531b807c14047a
Shubham Gupta <shubhamgupto@gmail.com>
2024-04-03 18:03:55 -0400

    added nvidia jetson hw support

--- causal-conv1d/setup.py
+++ causal-conv1d/setup.py
@@ -104,10 +104,13 @@ if not SKIP_CUDA_BUILD:
                 "Note: make sure nvcc has a supported version by running nvcc -V."
             )
 
-    cc_flag.append("-gencode")
     cc_flag.append("arch=compute_70,code=sm_70")
     cc_flag.append("-gencode")
+    cc_flag.append("arch=compute_72,code=sm_72")
+    cc_flag.append("-gencode")
     cc_flag.append("arch=compute_80,code=sm_80")
+    cc_flag.append("-gencode")
+    cc_flag.append("arch=compute_87,code=sm_87")
     if bare_metal_version >= Version("11.8"):
         cc_flag.append("-gencode")
         cc_flag.append("arch=compute_90,code=sm_90")
